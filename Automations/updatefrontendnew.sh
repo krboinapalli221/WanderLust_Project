75109a5a589143cbbb05f137784eccab9b0c4ed9
krboinapalli <koteswarb221@gmail.com>
2024-10-02 12:03:20 +0530

    Initial commit

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -4,7 +4,7 @@
 INSTANCE_ID="i-01946072889ef50ed"
 
 # Retrieve the public IP address of the specified EC2 instance
-ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text --region us-east-2)
 
 # Path to the .env file
 file_to_find="../frontend/.env.docker"
